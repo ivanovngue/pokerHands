145af3be48c6f77fea4e6e1c48b0fc0542d8357b
Ivan <ivanovngue@gmail.com>
2021-09-12 11:59:06 +0400

    test that "GetGamesWithTwoPlayersFromTxtFile" select the correct rows. Test fail.

---
+++ src/main/java/com/iv/kata/pk/infrastructure/RetrieveInputData.java
@@ -0,0 +1,12 @@
+package com.iv.kata.pk.infrastructure;
+
+import java.util.List;
+
+/**
+ * This class is used to get data
+ *
+ * @author Ivan
+ */
+public class RetrieveInputData {
+    public List<String> getGamesWithTwoPlayersFromTxtFile() {return null;}
+}
