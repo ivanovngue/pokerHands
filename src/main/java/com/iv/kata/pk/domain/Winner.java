05d17439bd16c1fa99dd7615efe3002db67c5051
Ivan <ivanovngue@gmail.com>
2021-09-12 12:41:29 +0400

    full implementation. main class can displays winners.

--- src/main/java/com/iv/kata/pk/domain/Winner.java
+++ src/main/java/com/iv/kata/pk/domain/Winner.java
@@ -1,7 +1,9 @@
 package com.iv.kata.pk.domain;
 
+import com.iv.kata.pk.domain.dto.Player;
 import com.iv.kata.pk.infrastructure.RetrieveInputData;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,12 +15,55 @@ public class Winner {
 
     private RetrieveInputData retrieveInputData = new RetrieveInputData();
 
+    private Ranking ranking = new Ranking();
+    private Compare compare = new Compare();
+
+    private static final String WINS = " wins.";
+    private static final String TIE = "Tie.";
+
 
     public List<String> getWinner() {
         return findWinnerBetweenTwoPlayer(retrieveInputData.getGamesWithTwoPlayersFromTxtFile());
     }
 
     private List<String> findWinnerBetweenTwoPlayer(List<String> twoPlayers) {
-        return null;
+        List<String> result = new ArrayList<>();
+
+        for (String twoPlayer : twoPlayers) {
+            String blackPlayerString = twoPlayer.substring(0, twoPlayer.length() / 2);
+            String whitePlayerString = twoPlayer.substring(twoPlayer.length() / 2 + 1);
+            Player blackPlayer = new Player(blackPlayerString, "Black");
+            Player whitePlayer = new Player(whitePlayerString, "White");
+
+            int blackPlayerRank = ranking.getPlayerRanking(blackPlayer).getRank();
+            int whitePlayerRank = ranking.getPlayerRanking(whitePlayer).getRank();
+
+            String blackPlayerMessageDetail = ranking.getPlayerRanking(blackPlayer).getMessageDetail();
+            String whitePlayerMessageDetail = ranking.getPlayerRanking(whitePlayer).getMessageDetail();
+
+            if (blackPlayerRank > whitePlayerRank) {
+                result.add(blackPlayer.getName() + WINS + " - with " + blackPlayerMessageDetail);
+            } else if (blackPlayerRank == whitePlayerRank) {
+                switch (blackPlayerRank) {
+                    case 0:
+                        addResult(result, compare.comparePlayerWithHigh(blackPlayer, whitePlayer), blackPlayerMessageDetail);
+                        break;
+                    default:
+                        result.add(TIE);
+                        break;
+                }
+            } else {
+                result.add(whitePlayer.getName() + WINS + " - with " + whitePlayerMessageDetail);
+            }
+        }
+        return result;
+    }
+
+    private void addResult(List<String> result, Player player, String messageDetail) {
+        if (player != null) {
+            result.add(player.getName() + WINS + " - with " + messageDetail);
+        } else {
+            result.add(TIE);
+        }
     }
 }
